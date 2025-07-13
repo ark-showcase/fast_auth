b4df65e2139f45e792aabc7a500813e68d3dd6e3
Ashiqur Rahman <ashiqur.rahman@amiprobashi.com>
2025-07-13 18:38:54 +0600

    modelize

--- app/models.py
+++ app/models.py
@@ -14,6 +14,20 @@ class RoutePermission(Base):
     path = Column(String(255), unique=True, index=True)
     required_role = Column(String(50))
 
+
+class Users(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(255), unique=True, index=True)
+    password = Column(String(255))
+    group = Column(String(50))
+
+
+class UserGroups(Base):
+    __tablename__ = "user_groups"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(255), unique=True, index=True)
+
 # For dev/demo purposes (still in memory)
 fake_users_db: Dict[str, dict] = {
     "admin_user": {
--- app/users.py
+++ app/users.py
@@ -1,12 +1,21 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends
 from app.schemas import UserCreate
 from app.utils import get_password_hash
 from app.models import fake_users_db, fake_groups_db
+from models import SessionLocal, Users, UserGroups
+from sqlalchemy.orm import Session
 
 users_router = APIRouter()
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 @users_router.post("/signup")
-def signup(user: UserCreate):
+def signup(user: UserCreate, db: Session = Depends(get_db)):
     if user.username in fake_users_db:
         raise HTTPException(status_code=400, detail="Username already exists")
 
@@ -14,6 +23,20 @@ def signup(user: UserCreate):
         raise HTTPException(status_code=400, detail="Invalid group")
 
     hashed_password = get_password_hash(user.password)
+
+    existing_user_by_requested_username = db.query(Users).filter_by(username=user.username).first()
+    if existing_user_by_requested_username:
+        raise HTTPException(status_code=400, detail="Username already exists")
+
+    requested_user_group = db.query(UserGroups).filter_by(title=user.group).all()
+    if not requested_user_group:
+        raise HTTPException(status_code=400, detail="Invalid group")
+
+    new_user = Users(username=user.username, password=hashed_password, group=user.group)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
     fake_users_db[user.username] = {
         "username": user.username,
         "password": hashed_password,

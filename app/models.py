1b48b201395944169b2fb5a93c55465215e41aa5
MD Ashiqur Rahman Khan <arkhan.private@gmail.com>
2025-07-14 23:10:59 +0600

    .env

--- app/models.py
+++ app/models.py
@@ -1,9 +1,13 @@
 from typing import Dict
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine, Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:123456@localhost:3306/getauth"
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
